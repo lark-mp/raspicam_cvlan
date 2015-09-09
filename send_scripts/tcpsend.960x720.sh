03c2861ae9b7468839a80984bab35fe658673196
Lark_mp <lark.mistyphantom@gmail.com>
2015-09-09 14:16:44 +0000

    Removed unnecessary script

--- send_scripts/tcpsend.960x720.sh
+++
@@ -1,9 +0,0 @@
-CAM_WIDTH=960
-CAM_HEIGHT=720
-CAM_FRAMERATE=30
-
-ENCODE_BITRATE=10000000
-
-HOST_IP=192.168.10.100
-
-./raspicam_cvlan -l | gst-launch-1.0 fdsrc ! videoparse format=GST_VIDEO_FORMAT_I420 width=${CAM_WIDTH} height=${CAM_HEIGHT} framerate=${CAM_FRAMERATE}/1 ! omxh264enc target-bitrate=${ENCODE_BITRATE} control-rate=variable ! video/x-h264,width=${CAM_WIDTH},height=${CAM_HEIGHT},stream-format=byte-stream,profile=high ! h264parse ! tcpclientsink host=${HOST_IP} port=5001 sync=false
